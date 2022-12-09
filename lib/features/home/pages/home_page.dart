f33290d8023c51f012b8aa90215c072fb8acc019
Adrian Bugała <adbugala@gmail.com>
2022-12-09 14:40:07 +0100

    Add decoration to Dismissible

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -47,10 +47,28 @@ class HomePage extends StatelessWidget {
                 children: [
                   for (final bookModel in state.bookModel)
                     Dismissible(
+                      direction: DismissDirection.endToStart,
                       key: ValueKey(bookModel.id),
                       onDismissed: (direction) {
                         context.read<HomeCubit>().remove(id: bookModel.id);
                       },
+                      background: const Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 32.0),
+                              child: Icon(
+                                Icons.delete,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                       child: BookThumbnail(
                         bookModel: bookModel,
                       ),
