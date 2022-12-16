d3f63520552ffe36db17e31b7ad4febc6da06685
Adrian Bugała <adbugala@gmail.com>
2022-12-16 12:58:46 +0100

    Extract BookPopupMenuButton to another file

---
+++ lib/features/home/pages/book_popup_menu_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:my_books/domain/models/book_model.dart';
+import 'package:my_books/features/add/add_current_page.dart';
+import 'package:my_books/features/edit/pages/edit_page.dart';
+import 'package:my_books/features/remove/pages/remove_page.dart';
+
+class BookPopupMenuButton extends StatelessWidget {
+  const BookPopupMenuButton({
+    Key? key,
+    required this.bookModel,
+  }) : super(key: key);
+
+  final BookModel bookModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: (context) => [
+        const PopupMenuItem(
+          value: 1,
+          child: Text('Add current page'),
+        ),
+        const PopupMenuItem(
+          value: 2,
+          child: Text('Edit'),
+        ),
+        const PopupMenuItem(
+          value: 3,
+          child: Text('Delete'),
+        ),
+      ],
+      onSelected: (value) {
+        if (value == 1) {
+          showDialog(
+              context: context,
+              builder: (context) => AddCurrentPage(
+                    bookModel: bookModel,
+                  ));
+        }
+        if (value == 2) {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (_) => EditPage(
+                    bookModel: bookModel,
+                  )));
+        }
+        if (value == 3) {
+          showDialog(
+              context: context,
+              builder: (context) => RemoveDialog(
+                    bookModel: bookModel,
+                  ));
+        }
+      },
+    );
+  }
+}
--- lib/features/home/pages/book_thumbnail.dart
+++ lib/features/home/pages/book_thumbnail.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_books/domain/models/book_model.dart';
-import 'package:my_books/features/add/add_current_page.dart';
 import 'package:my_books/features/details/details_page.dart';
-import 'package:my_books/features/edit/pages/edit_page.dart';
-import 'package:my_books/features/remove/pages/remove_page.dart';
+import 'package:my_books/features/home/pages/book_popup_menu_button.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class BookThumbnail extends StatelessWidget {
@@ -104,44 +102,7 @@ class BookThumbnail extends StatelessWidget {
                                 ],
                               ),
                             ),
-                            PopupMenuButton(
-                              itemBuilder: (context) => [
-                                const PopupMenuItem(
-                                  value: 1,
-                                  child: Text('Add current page'),
-                                ),
-                                const PopupMenuItem(
-                                  value: 2,
-                                  child: Text('Edit'),
-                                ),
-                                const PopupMenuItem(
-                                  value: 3,
-                                  child: Text('Delete'),
-                                ),
-                              ],
-                              onSelected: (value) {
-                                if (value == 1) {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => AddCurrentPage(
-                                            bookModel: bookModel,
-                                          ));
-                                }
-                                if (value == 2) {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (_) => EditPage(
-                                            bookModel: bookModel,
-                                          )));
-                                }
-                                if (value == 3) {
-                                  showDialog(
-                                      context: context,
-                                      builder: (context) => RemoveDialog(
-                                            bookModel: bookModel,
-                                          ));
-                                }
-                              },
-                            ),
+                            BookPopupMenuButton(bookModel: bookModel),
                           ],
                         ),
                       ),
