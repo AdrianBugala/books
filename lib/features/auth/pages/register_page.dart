02ab769c65d10dab39ed33003b83a698a23e5901
AdrianBugala <adbugala@gmail.com>
2023-03-17 15:15:42 +0100

    Add register functionality
    Add new looks to register page fitted to new login page

--- lib/features/auth/pages/register_page.dart
+++ lib/features/auth/pages/register_page.dart
@@ -1,17 +1,144 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_books/features/auth/pages/widgets/auth_button.dart';
+import 'package:my_books/features/auth/pages/widgets/auth_text_field.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({
     required this.onTap,
     super.key,
   });
   final Function()? onTap;
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+// todo nie działa guzik rejestracji
+  void signUserUp() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+    try {
+      if (passwordController.text == confirmPasswordController.text) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+      } else {
+        Navigator.pop(context);
+        showErrorMessage('Password don\' match');
+        return;
+      }
+
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (error) {
+      Navigator.pop(context);
+      showErrorMessage(error.message.toString());
+    }
+  }
+
+  void showErrorMessage(String text) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xff326789),
+          title: Center(
+            child: Text(text, style: const TextStyle(color: Colors.white)),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: InkWell(
-        onTap: onTap,
-        child: const Text('register page'),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(204, 120, 165, 200),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                const Icon(
+                  Icons.menu_book_rounded,
+                  size: 200,
+                  color: Color(0xffe9eef2),
+                ),
+                const SizedBox(height: 25),
+
+                // welcome message
+                const Text(
+                  'Let\'s create an account for you!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff326789),
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                // username textfield
+                AuthTextField(
+                  controller: emailController,
+                  hintText: 'User name',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 5),
+                AuthTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 5),
+                AuthTextField(
+                  controller: confirmPasswordController,
+                  hintText: 'Confirm password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 25),
+
+                // button
+                AuthButton(
+                  onTap: signUserUp,
+                  text: 'Sign Up',
+                ),
+                const SizedBox(height: 50),
+
+                // not a member? register now!
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Already have an account?'),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    InkWell(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Login now!',
+                        style: TextStyle(
+                          color: Color(0xff326789),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
