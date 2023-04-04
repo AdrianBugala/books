97b435215bdffef0fb461263151e18b67e87d453
AdrianBugala <adbugala@gmail.com>
2023-04-04 12:53:59 +0200

    Fix the problem with getting invalid BloC imports

--- lib/features/edit/cubit/edit_cubit.dart
+++ lib/features/edit/cubit/edit_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:my_books/domain/models/book_model.dart';
@@ -6,7 +6,8 @@ import 'package:my_books/domain/repositories/book_repository.dart';
 
 part 'edit_state.dart';
 part 'edit_cubit.freezed.dart';
-@injectable  
+
+@injectable
 class EditCubit extends Cubit<EditState> {
   EditCubit(this._bookRepository) : super(const EditState());
 
